ea181be33b5d0f84fcd3f0d603efd379cb969c0f
Pete "Peteches" McCabe <configs@petemccabe.gb.net>
2013-08-14 16:56:27 +0100

    modified setup and added new X configs
    - added xsession start up script
    - added xdefaults config file
    - removed powerline installation aspects of setup due to issues with git repo holding git repo. will add script to automate fresh clone.. later

--- setup.sh
+++ setup.sh
@@ -47,44 +47,18 @@ if [[ -n $_install ]]; then
 	ln -s ${config_dir}/tmux.conf ~/.tmux.conf
 fi
 
-read -p "Install Powerline? [y/N]" INSTALL_POWERLINE
-
-if [[ $INSTALL_POWERLINE -eq y ]]; then
-
-	cd $config_dir/powerline-srcs
-	echo "Updating sources:"
-	for i in *; do
-		echo "updating $i"
-		cd $i && git pull && cd -
-	done
-
-	echo "installing powerline"
-	cd powerline/ && \
-	sudo python setup.py install && \
-	cd -
-	[[ $? -ne 0 ]] &&  echo "Failed" && exit 1
-
-	echo "Installing powerline-daemon"
-	cd powerline-daemon/ && \
-	sudo python setup.py install && \
-	cd -
-	[[ $? -ne 0 ]] &&  echo "Failed" && exit 1
-
-	[[ -d ~/.fonts ]] || mkdir -p ~/.fonts
-
-	echo "linking in powerline fonts"
-	cd $config_dir/powerline-fonts
-	for i in *;do 
-		[[ -L ~/.fonts/${i} ]] && rm -fr ~/.fonts/${i}
-		ln -sf $config_dir/powerline-fonts/$i ~/.fonts/$i
-	done
-	echo "refreshing font cache"
-	fc-cache -vf ~/.fonts
-
-
-	[[ -d ~/.config/powerline || -L ~/.config/powerline ]] && rm -rf ~/.config/powerline
-	if [[ -n $_install ]]; then
-		ln -s ${config_dir}/powerline ~/.config/powerline
-	fi
-
+[[ -f ~/.xsession ]] && rm -f ~/.xsession
+if [[ -n $_install ]]; then
+	ln -s ${config_dir}/X-configs/xsession ~/.xsession
 fi
+
+[[ -f ~/.Xdefaults ]] && rm -f ~/.Xdefaults
+if [[ -n $_install ]]; then
+	ln -s ${config_dir}/X-configs/Xdefaults ~/.Xdefaults
+fi
+
+[[ -d ~/.config/powerline || -L ~/.config/powerline ]] && rm -rf ~/.config/powerline
+if [[ -n $_install ]]; then
+	ln -s ${config_dir}/powerline ~/.config/powerline
+fi
+
