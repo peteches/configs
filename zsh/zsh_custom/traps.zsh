5c0ebfd6cb22d51277a92b90b0f12552d6e5dcad
Pete "Peteches" McCabe <configs@petemccabe.gb.net>
2014-12-22 09:38:00 +0000

    updated ackbar trap to return correct error code

--- zsh/zsh_custom/traps.zsh
+++ zsh/zsh_custom/traps.zsh
@@ -1,7 +1,10 @@
 # vim : set ft=bash
 ## It's a TRAP
 if [[ -x =ack ]]; then
-    trap "echo;ack --bar | sed 'y/ge/ta/'" SIGINT
+	TRAPINT() {
+		echo;ack --bar | sed "y/ge/ta/"
+		return $(( 128 + $1 ))
+	}
 fi
 
 
